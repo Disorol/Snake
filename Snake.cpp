9f2b16e6c7f21ff042484a0017382ffb450838d6
Disorol <49810312+Disorol@users.noreply.github.com>
2020-01-18 14:22:47 +0500

    Adding logic

--- Snake.cpp
+++ Snake.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
@@ -12,23 +13,79 @@ eDirection dir;
 void Setup() {
     gameOver = false;
     dir = STOP;
-    x = width / 2;
-    y = height / 2;
+    x = width / 2 - 1;
+    y = height / 2 - 1;
     fruit_x = rand() % width;
-    fruit_y = rand() % height;
+    fruit_x = rand() % height;
     score = 0;
 }
 
 void Draw() {
+    system("cls");
+    for (int i = 0; i < width + 1; i++) {
+        cout << "#";
+    }
+    cout << endl;
 
+    for (int i = 0; i < height; i++) {
+        for (int f = 0; f < width; f++) {
+            if (f == 0 || f == width - 1)
+                cout << "#";
+            if (i == y && f == x)
+                cout << "0";
+            else if (i == fruit_y && f == fruit_x)
+                cout << "F";
+            else 
+                cout << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width + 1; i++) {
+        cout << "#";
+    }
+    cout << endl;
 }
 
 void Input() {
-
+    if (_kbhit ()) {
+        switch (_getch ())
+        {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        }
+    }
 }
 
 void Logic() {
-
+    switch (dir)
+    {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        break;
+        x++;
+    case UP:
+        break;
+        y--;
+    case DOWN:
+        y++;
+        break;
+    }
 }
 
 int main() {
